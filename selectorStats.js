006c002db603ab7c390c3719c0bbe9a1a37fdfdc
deathy <deathy@deathy.ro>
2015-01-17 17:01:38 +0200

    Initial version, small CSS selector stats including unused and times for selector query

---
+++ selectorStats.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function RunSelectorStats() {
+
+    var result = {};
+    var unused_selectors = [];
+    result.unused_selectors = unused_selectors;
+    var selector_times = [];
+    result.selector_times = selector_times;
+    var sheets = document.styleSheets;
+    var sheetsLength = sheets.length;
+    for (var i = 0; i < sheetsLength; i++) {
+        var sheetRules = sheets[i].rules;
+        if (sheetRules != null) {
+            var sheetRulesLength = sheetRules.length;
+            for (var j = 0; j < sheetRulesLength; j++) {
+                var sheetRule = sheetRules[j];
+                var selectorText = sheetRule.selectorText;
+                var startTime = new Date;
+                var selectorResults = document.querySelectorAll(selectorText);
+                if (selectorResults.length == 0) {
+                    unused_selectors.push(selectorText);
+                }
+                var time = new Date - startTime;
+                selector_times.push({selector_text: selectorText, selector_time: time});
+            }
+        }
+    }
+    return result;
+}
